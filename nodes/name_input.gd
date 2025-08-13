baa0da325ae616f151a07d0444cfab896e2259c8
AyaPK <Sherbnya@gmail.com>
2025-08-13 23:49:20 +0100

    Resets player name to default

--- nodes/name_input.gd
+++ nodes/name_input.gd
@@ -4,7 +4,7 @@ var letters := ["A","B","C","D","E","F","G","H","I","J","K","L","M",
 				"N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 				"0","1","2","3","4","5","6","7","8","9"," "]
 
-var player_name := ["A", "Y", "A"]
+var player_name := ["A", "A", "A"]
 
 var current_pos := 0
 
